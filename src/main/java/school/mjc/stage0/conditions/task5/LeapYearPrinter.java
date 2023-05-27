c5aa4266e84d4f79c8d23e0c57aeb0a52cdde5c6
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-27 18:05:34 +0300

    finish task 4 module 4.1 stage 0

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year % 4){
+            default:
+                System.out.println("not leap");
+                break;
+            case  0:
+                if (year % 100 != 0 || year % 400 == 0) {
+                    System.out.println("leap");
+                } else {
+                    System.out.println("not leap");
+                }
+                break;
+            }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter leapPrinter = new LeapYearPrinter();
+
+        leapPrinter.isLeapYear(1900);
     }
 }
